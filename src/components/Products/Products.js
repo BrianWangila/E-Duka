7e0863f2f46eba77e8249b5dfba29216c0d4ebb7
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-13 02:20:37 +0300

    render the products in the homepage and style the cards

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getAllProducts } from "../../API/Api";
-import { List, Card, Image } from "antd";
+import { List, Card, Image, Typography } from "antd";
+const { Meta } = Card;
 
 const Products = () => {
   const [items, setItems] = useState([]);
@@ -14,14 +15,43 @@ const Products = () => {
   return (
     <div className="products">
       <List
-      grid={{column: 3}}
+        grid={{ column: 3 }}
         renderItem={(product, index) => {
           return (
             <Card
               title={product.title}
               key={index}
-              cover={<Image className="item-card-image" src={product.thumbnail} />}
-            ></Card>
+              cover={
+                <Image className="item-card-image" src={product.thumbnail} />
+              }
+            >
+              <Card.Meta
+                title={
+                  <Typography.Paragraph>
+                    Price: ${product.price}
+                    <Typography.Text delete type="danger">
+                      {" "}
+                      $
+                      {parseFloat(
+                        product.price /
+                          (1 - product.discountPercentage / 100).toFixed(2)
+                      ).toFixed(2)}
+                    </Typography.Text>
+                  </Typography.Paragraph>
+                }
+                description={
+                  <Typography.Paragraph
+                    ellipsis={{
+                      rows: 2,
+                      expandable: true,
+                      symbol: "Show More",
+                    }}
+                  >
+                    {product.description}
+                  </Typography.Paragraph>
+                }
+              ></Card.Meta>
+            </Card>
           );
         }}
         dataSource={items}
