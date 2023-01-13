05c80ad89495e77223a8e6eb91487edc404dba4f
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-13 16:54:00 +0300

    add footer details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
+import { Typography } from "antd";
 import React from "react";
 
 const Footer = () => {
-  return <div className="appFooter">Header</div>;
+  return (
+    <div className="appFooter">
+      <div className="footer-links">
+        <Typography.Link>Privacy Policy</Typography.Link>
+        <Typography.Link>Terms of Service</Typography.Link>
+        <Typography.Link>Support</Typography.Link>
+      </div>
+
+      <Typography.Text>© 2023</Typography.Text>
+    </div>
+  );
 };
 
 export default Footer;
