0229560a57a7d355d17ddd3adce8a60227331e25
Tonny Oseko <tonnyoseko0@gmail.com>
2023-01-26 01:30:34 +0300

    add a register page

--- src/components/Routes/AppRoutes.js
+++ src/components/Routes/AppRoutes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Category from "../../Pages/Category/Category";
 import Home from "../../Pages/Home";
+import Register from "../../Pages/Register/Register";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/:categoryId" element={<Category />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
